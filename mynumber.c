303216b7f7561684c1ad0916b10658b478ad5109
Ladea1224 <ladea1224@pusan.ac.kr>
2026-04-29 12:12:08 +0900

    prime Number Added

--- mynumber.c
+++ mynumber.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-	int i;
+	int i,j;
 	for(i=1;i<=100;++i){
 		if(i%2!=0) printf("%d ",i);
 	}
@@ -8,5 +8,17 @@ int main(){
 	for(i=1;i<=100;++i){
 		if(i%2==0) printf("%d ",i);
 	}
+	printf("\n");
+	for(i=2;i<=100;++i){
+		bool is_prime=true;
+		for(j=2;j*j<=i;++j){
+			if(i%j==0){
+				is_prime=fasle;
+				break;
+			}
+		}
+		if(is_prime) printf("%d " , i);
+	}
+		
 	return 0;
 }
